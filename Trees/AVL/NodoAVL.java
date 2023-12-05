790737f5bc59314da6953b9d939060bf65e43ab5
Joshu-13 <112602508+Joshu-13@users.noreply.github.com>
2023-12-04 21:07:35 -0600

    Update NodoAVL.java

--- Trees/AVL/NodoAVL.java
+++ Trees/AVL/NodoAVL.java
@@ -1,12 +1,12 @@
 package Trees.AVL;
-
+// Definición de la clase NodoAVL
 public class NodoAVL {
     int clave, altura;
     NodoAVL hijoizquierdo, hijoderecho;
 
+    // Constructor de la clase NodoAVL
     NodoAVL(int valor) {
         this.clave = valor;
-        // this.fe = 1;
         altura = 1;
     }
 }
